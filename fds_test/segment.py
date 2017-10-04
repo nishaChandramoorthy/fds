ef55720c032f64bc8d5b107260dc0215951f5b8c
Nisha Chandramoorthy <nishac92@gmail.com>
2017-10-03 21:24:08 -0400

    move segment to actual file

--- fds_test/segment.py
+++ fds_test/segment.py
@@ -39,11 +39,17 @@ def run_segment(run, u0, V, v, parameter, i_segment, steps,
     # run homogeneous tangents
     res_h = []
     subspace_dimension = len(V)
+   
+ 
+    nparams = len(parameter)
+    u1i = []
+    res_i = []
 
-    u1i = u0 + v * epsilon
-    run_ids = ['segment{0:02d}_param_perturb{1:03d}'.format(
-             i_segment, subspace_dimension)]
-    u1i.value.field = os.path.join(get_host_dir(run_ids[0]), 'input.h5')
+    for i in range(nparams):
+        u1i.append(u0 + v[i] * epsilon)
+        run_ids.append('segment{0:02d}_param_perturb{1:03d}'.format(i_segment, i))
+        u1i[-1].value.field = os.path.join(get_host_dir(run_ids[-1]), 'input.h5')
+ 
     u1h = []
     for j in range(subspace_dimension):
         u1h.append(u0 + V[j] * epsilon)
@@ -51,7 +57,7 @@ def run_segment(run, u0, V, v, parameter, i_segment, steps,
         u1h[-1].value.field = os.path.join(get_host_dir(run_ids[-1]), 'input.h5')
 
     # compute all outputs
-    run_compute([u1i] + u1h + compute_outputs, spawn_compute_job=spawn_compute_job, interprocess=interprocess)
+    #run_compute(u1i + u1h + compute_outputs, spawn_compute_job=spawn_compute_job, interprocess=interprocess)
 
     res_0 = threads.apply_async(
             run, (u0.field, parameter, steps, run_id, interprocess))
@@ -59,10 +65,14 @@ def run_segment(run, u0, V, v, parameter, i_segment, steps,
     for j in range(subspace_dimension):
         res_h.append(threads.apply_async(
             run, (u1h[j].field, parameter, steps, run_ids[1+j], interprocess)))
-    # run inhomogeneous tangent
-    res_i = threads.apply_async(
-            run, (u1i.field, parameter + epsilon, steps, run_ids[0], interprocess))
-
+    # run inhomogeneous tangents
+    epsilon_j = zeros_like(parameter)
+    for j in range(nparams):
+        epsilon_j[j] = epsilon
+        res_i.append(threads.apply_async(
+            run, (u1i[j].field, parameter + epsilon_j, steps, run_ids[1+j], interprocess)))
+        epsilon_j[j] = 0.e0
+    
     u0p, J0 = res_0.get()
     u0p = pascal.symbolic_array(field=u0p)
     # get homogeneous tangents
@@ -73,10 +83,14 @@ def run_segment(run, u0, V, v, parameter, i_segment, steps,
         u1p = pascal.symbolic_array(field=u1p)
         V[j] = (u1p - u0p) / epsilon
         G.append(trapez_mean(J1 - J0, 0) / epsilon)
-    # get inhomogeneous tangent
-    u1p, J1 = res_i.get()
-    u1p = pascal.symbolic_array(field=u1p)
-    v, g = (u1p - u0p) / epsilon, trapez_mean(J1 - J0, 0) / epsilon
+    # get inhomogeneous tangents
+    v = pascal.random(nparams)
+    g = []
+    for j in range(nparams):
+        u1p, J1 = res_i[j].get()
+        u1p = pascal.symbolic_array(field=u1p)
+        v[j] = (u1p - u0p) / epsilon 
+        g.append(trapez_mean(J1 - J0, 0) / epsilon)
     threads.close()
     threads.join()
     return u0p, V, v, J0, G, g
